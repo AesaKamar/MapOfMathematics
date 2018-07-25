2b99f747c373eafc3b8041c23674d20ac5c29665
AesaKamar <aesa.kamar@gmail.com>
2018-07-25 07:55:31 -0400

    move JS and deduplicate edges and nodes

--- src/test/scala/example/ParserTest.scala
+++ src/test/scala/example/ParserTest.scala
@@ -9,8 +9,10 @@ import scala.concurrent.Future
 class ScraperTest extends AsyncFreeSpec with Matchers {
   import Scraper._
   import io.circe._, io.circe.generic.auto._, io.circe.syntax._
+  import Inspectors._
 
   def parseHtmlToEntryNodesAndEdges(fileName: String): (List[SigmaNode], List[SigmaEdge]) = {
+    println(fileName)
     val entryNodes = parseFile(fileName)
       .map(MathSciNetEntry.fromTuple)
 
@@ -30,15 +32,22 @@ class ScraperTest extends AsyncFreeSpec with Matchers {
       .filter(x => x.endsWith("html"))
       .sorted
 
+    val sigmaRoot = SigmaNode(".", "Truth", 0, 0, 3)
     val parseResults =
       paths.map(parseHtmlToEntryNodesAndEdges).unzip match {
-        case (nodes, edges) => (nodes.flatten.asJson, edges.flatten.asJson)
+        case (nodes, edges) => (nodes.flatten.toSet.+(sigmaRoot), edges.flatten.toSet)
       }
+    val (nodes, edges) = parseResults
 
-    val (nodesJson, edgesJson) = parseResults
+    val missingSources = edges
+      .flatMap(e => nodes.filterNot(_.id == e.source).headOption.map(_ => e))
+    val missingTargets = edges
+      .flatMap(e => nodes.filterNot(_.id == e.target).headOption.map(_ => e))
 
-    file"docs/nodes.json" < nodesJson.toString
-    file"docs/edges.json" < edgesJson.toString
+    val edgesWithSourceAndTarget = edges -- (missingSources ++ missingTargets)
+
+    file"docs/nodes.json" < nodes.asJson.toString
+    file"docs/edges.json" < edgesWithSourceAndTarget.asJson.toString
 
     Future(succeed)
   }
